967cfa73bed72beae301df84cec1367ea990c25b
James Skarie <jskarie@gmail.com>
2015-08-19 15:55:11 -0500

    added limited image_sprite support

--- pipeline_compass/compiler.py
+++ pipeline_compass/compiler.py
@@ -1,9 +1,9 @@
+import os
+import scss
+
 from pipeline.compilers import CompilerBase
 from django.utils.encoding import smart_str
 from django.conf import settings
-
-import scss
-import os
 
 
 def add_to_scss_path(path):
@@ -34,6 +34,16 @@ class CompassCompiler(CompilerBase):
         return open(path, 'w').write(smart_str(content))
 
 # setup scss load path
+scss.config.STATIC_ROOT = settings.STATIC_ROOT
+if (hasattr(settings, 'SCSS_STATIC_ROOT')):
+  scss.config.ASSETS_ROOT = settings.SCSS_STATIC_ROOT
+  if not os.path.exists(settings.SCSS_STATIC_ROOT):
+    os.makedirs(settings.SCSS_STATIC_ROOT)
+
+scss.config.STATIC_URL = settings.STATIC_URL
+scss.config.ASSETS_URL = settings.STATIC_URL
+ 
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 scss_path = os.path.join(root, 'pipeline_compass', 'compass')
 add_to_scss_path(scss_path)
+
